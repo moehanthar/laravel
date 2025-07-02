26967184af8cb9242a06adad6e47edcfa2d3c465
moehanthar <moehanthan6@gmail.com>
2025-07-02 19:52:53 +0630

    laravel1

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/intros',[App\Http\Controller\IntroController::class,'show']);//classထဲမှာရှိတဲ့functionတွေကိုmethodလို့ခေါ်တယ်
